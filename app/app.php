8fa7542b6afce64d31b5f39c95483b37156e4638
Jon Sweeney <jonsweeney26@gmail.com>
2016-02-19 11:38:46 -0800

    add conditions for 0, 1 or more count values

--- app/app.php
+++ app/app.php
@@ -15,7 +15,13 @@
     $app->get("/count", function() use ($app) {
         $my_CountRepeats = new CountRepeats;
         $my_count = $my_CountRepeats->count($_GET['phrase'], $_GET['word']);
-        $message_text = "The word " . $_GET['word'] . " appears " . $my_count . " times.";
+        if($my_count == 0) {
+            $message_text = $_GET['word'] . " does not appear in the phrase: " . $_GET['phrase'];
+        } elseif($my_count == 1) {
+            $message_text = "The word " . $_GET['word'] . " appears 1 time.";
+        } else {
+            $message_text = "The word " . $_GET['word'] . " appears " . $my_count . " times.";
+        }
         return $app['twig']->render('count.html.twig', array(
             'form' => true,
             'navbar' => true,
